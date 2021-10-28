6b6223db6a55edaf0dfcfcf1e349d244acc193db
Kevin Barabash <kevinb@khanacademy.org>
2021-10-28 11:52:26 -0400

    don't test #length

--- src/__tests__/vector.test.js
+++ src/__tests__/vector.test.js
@@ -1,14 +1,14 @@
 const Vector = require("../vector.js");
 
-describe("#length", () => {
-    it.each`
-        vector | length
-        ${new Vector(0, 0)} | ${0}
-        ${new Vector(3, 4)} | ${5}
-    `(`$vector.length = $length`, ({vector, length}) => {
-        expect(vector.length).toEqual(length);
-    });
-});
+// describe("#length", () => {
+//     it.each`
+//         vector | length
+//         ${new Vector(0, 0)} | ${0}
+//         ${new Vector(3, 4)} | ${5}
+//     `(`$vector.length = $length`, ({vector, length}) => {
+//         expect(vector.length).toEqual(length);
+//     });
+// });
 
 describe("#toString()", () => {
     it("should return the correct string", () => {
